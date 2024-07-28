92eaa1b2ae6a25fb8c62973ad7c31e55d66e4639
Faisal Karim <miraklasiaf@gmail.com>
2024-07-28 11:09:17 +0700

    Experiment to measure the difference in running time between our potentially inefficient versions and the one that uses strings.Join
    Signed-off-by: Faisal Karim <miraklasiaf@gmail.com>

---
+++ ch1/tst1-3.go
@@ -0,0 +1,11 @@
+/*
+ * Experiment to measure the difference in running time
+ * between our potentially inefficient versions and the one that uses strings.Join.
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("todo")
+}
